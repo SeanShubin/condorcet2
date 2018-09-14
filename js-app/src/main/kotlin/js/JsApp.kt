ce68ee59814a4510aff73e84b307bba69a56972e
Sean Shubin <sshubin@cj.com>
2018-09-14 16:37:36 -0700

    refactoring

--- js-app/src/main/kotlin/js/JsApp.kt
+++ js-app/src/main/kotlin/js/JsApp.kt
@@ -2,32 +2,39 @@ package js
 
 import com.seanshubin.condorcet.common.backend.Condorcet
 import com.seanshubin.condorcet.js.SampleData
-import kotlinx.html.div
+import kotlinx.html.*
 import kotlinx.html.dom.append
-import kotlinx.html.h2
-import kotlinx.html.pre
+import org.w3c.dom.HTMLElement
 import kotlin.browser.document
 
+class App(private val root: HTMLElement, private val inputLines:List<String>){
+    fun input(): List<String> {
+        root.append {
+            h2 {
+                +"Input"
+            }
+            pre {
+                +inputLines.joinToString("\n")
+            }
+        }
+        return inputLines
+    }
+
+    fun output() {
+        val outputLines = Condorcet.processLines(inputLines)
+        root.append {
+            h2 {
+                +"Output"
+            }
+            pre {
+                +outputLines.joinToString("\n")
+            }
+        }
+    }
+}
+
 fun main(arguments: Array<String>) {
-    val condorcetDiv = document.body!!.append.div()
-
-    val inputLines = SampleData.sampleInput
-    condorcetDiv.append {
-        h2 {
-            +"Input"
-        }
-        pre {
-            +inputLines.joinToString("\n")
-        }
-    }
-
-    val outputLines = Condorcet.processLines(inputLines)
-    condorcetDiv.append {
-        h2 {
-            +"Output"
-        }
-        pre {
-            +outputLines.joinToString("\n")
-        }
-    }
+    val app = App(document.body!!.append.div(), SampleData.sampleInput)
+    app.input()
+    app.output()
 }
