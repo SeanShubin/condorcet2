c1f42cb7e47d1d8543c560a68024d91d025e0159
Sean Shubin <sshubin@cj.com>
2018-09-14 16:57:06 -0700

    login and register

--- js-app/src/main/kotlin/js/JsApp.kt
+++ js-app/src/main/kotlin/js/JsApp.kt
@@ -8,6 +8,18 @@ import org.w3c.dom.HTMLElement
 import kotlin.browser.document
 
 class App(private val root: HTMLElement, private val inputLines:List<String>){
+    fun style() {
+        root.append {
+            style {
+                +"""
+                    body {
+                        background-color: linen;
+                    }
+                """.trimIndent()
+            }
+        }
+    }
+
     fun input(): List<String> {
         root.append {
             h2 {
@@ -31,10 +43,59 @@ class App(private val root: HTMLElement, private val inputLines:List<String>){
             }
         }
     }
+
+    fun login() {
+        root.append {
+            fieldSet {
+                legend { +"login" }
+                div {
+                    +"EMail"
+                    input()
+                }
+                div {
+                    +"Password"
+                    passwordInput()
+                }
+                div {
+                    button {
+                        +"Login"
+                    }
+                }
+            }
+        }
+    }
+
+    fun register() {
+        root.append {
+            fieldSet {
+                legend { +"register" }
+                div {
+                    +"EMail"
+                    input()
+                }
+                div {
+                    +"Password"
+                    passwordInput()
+                }
+                div {
+                    +"Confirm Password"
+                    passwordInput()
+                }
+                div {
+                    button {
+                        +"Register"
+                    }
+                }
+            }
+        }
+    }
 }
 
 fun main(arguments: Array<String>) {
     val app = App(document.body!!.append.div(), SampleData.sampleInput)
+    app.style()
+    app.login()
+    app.register()
     app.input()
     app.output()
 }
