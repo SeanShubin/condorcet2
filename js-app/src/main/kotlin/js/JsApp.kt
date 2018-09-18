6deb0a0829bd2497416aecd41b1f0dd6f21c2fa2
Sean Shubin <sshubin@cj.com>
2018-09-17 17:22:39 -0700

    refactoring

--- js-app/src/main/kotlin/js/JsApp.kt
+++ js-app/src/main/kotlin/js/JsApp.kt
@@ -4,98 +4,87 @@ import com.seanshubin.condorcet.common.backend.Condorcet
 import com.seanshubin.condorcet.js.SampleData
 import kotlinx.html.*
 import kotlinx.html.dom.append
-import org.w3c.dom.HTMLElement
 import kotlin.browser.document
 
-class App(private val root: HTMLElement, private val inputLines:List<String>){
-    fun style() {
-        root.append {
-            style {
-                +"""
-                    body {
-                        background-color: linen;
-                    }
-                """.trimIndent()
+val inputLines = SampleData.sampleInput
+
+fun HtmlBlockTag.displayInputLines() {
+    h2 {
+        +"Input"
+    }
+    pre {
+        +inputLines.joinToString("\n")
+    }
+}
+
+fun HtmlBlockTag.displayOutputLines() {
+    val outputLines = Condorcet.processLines(inputLines)
+    h2 {
+        +"Output"
+    }
+    pre {
+        +outputLines.joinToString("\n")
+    }
+}
+
+fun HtmlBlockTag.login() {
+    fieldSet {
+        legend { +"login" }
+        div {
+            +"EMail"
+            input()
+        }
+        div {
+            +"Password"
+            passwordInput()
+        }
+        div {
+            button {
+                +"Login"
             }
         }
     }
+}
 
-    fun input(): List<String> {
-        root.append {
-            h2 {
-                +"Input"
+fun HtmlBlockTag.style() {
+    style {
+        +"""
+            body {
+                background-color: linen;
             }
-            pre {
-                +inputLines.joinToString("\n")
-            }
-        }
-        return inputLines
+        """.trimIndent()
     }
+}
 
-    fun output() {
-        val outputLines = Condorcet.processLines(inputLines)
-        root.append {
-            h2 {
-                +"Output"
-            }
-            pre {
-                +outputLines.joinToString("\n")
-            }
+fun HtmlBlockTag.register() {
+    fieldSet {
+        legend { +"register" }
+        div {
+            +"EMail"
+            input()
         }
-    }
-
-    fun login() {
-        root.append {
-            fieldSet {
-                legend { +"login" }
-                div {
-                    +"EMail"
-                    input()
-                }
-                div {
-                    +"Password"
-                    passwordInput()
-                }
-                div {
-                    button {
-                        +"Login"
-                    }
-                }
-            }
+        div {
+            +"Password"
+            passwordInput()
         }
-    }
-
-    fun register() {
-        root.append {
-            fieldSet {
-                legend { +"register" }
-                div {
-                    +"EMail"
-                    input()
-                }
-                div {
-                    +"Password"
-                    passwordInput()
-                }
-                div {
-                    +"Confirm Password"
-                    passwordInput()
-                }
-                div {
-                    button {
-                        +"Register"
-                    }
-                }
+        div {
+            +"Confirm Password"
+            passwordInput()
+        }
+        div {
+            button {
+                +"Register"
             }
         }
     }
 }
 
 fun main(arguments: Array<String>) {
-    val app = App(document.body!!.append.div(), SampleData.sampleInput)
-    app.style()
-    app.login()
-    app.register()
-    app.input()
-    app.output()
+    document.body!!.append.div() {
+        style()
+        login()
+        register()
+        displayInputLines()
+        displayOutputLines()
+    }
 }
